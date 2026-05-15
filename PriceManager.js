dfd565e92e12f1965c967fb15c004633d9d221da
donpu333 <donpupi61@gmail.com>
2026-05-15 19:04:12 +0300

    Update PriceManager.js

--- PriceManager.js
+++ PriceManager.js
@@ -31,7 +31,8 @@ class PriceManager {
             this.wsBinance = null;
         }
         
-        const ws = new WebSocket('wss://fstream.binance.com/ws/!miniTicker@arr');        
+        const ws = new WebSocket('wss://fstream.binance.com/ws/!ticker@arr');
+        
         // ФИЛЬТРЫ ДЛЯ ПЛАВНОСТИ ЦЕНЫ
         let _lastBatchUpdate = 0;
         const _lastPrices = new Map();
